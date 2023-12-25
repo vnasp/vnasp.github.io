3029daa049575a321e0c41298c9bb95060e3394a
Valentina Muñoz <valentinamr@gmail.com>
2023-12-25 17:16:32 -0300

    Update about me

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,25 @@
 
 const About = () => {
   return (
-    <div className="bg-indigo-400 h-auto p-4 text-white text-justify">
-      <h3 className="">Hola, ¿cómo estás?</h3>
-      <p className="font-extralight tracking-wide">Desarrolladora web con 10 años de experiencia en la creación y mantención de sitios web tipo e-commerce, blogs, landing paga; tengo conocimientos en lenguajes y herramientas tanto de frontend como de backend. Me considero responsable, proactiva, detalista y metódica</p>
+    <div className="flex flex-col gap-3 bg-indigo-400 h-auto p-8 text-white">
+      <div className="font-extralight tracking-wider text-justify">
+        Hola. ¿Qué tal? Mi nombre es Valentina. Hace más de 10 de años creo páginas web de manera freelance: desde los inicios del HTML hasta el actual framework React.JS. He desarrollado distintos sitios: informativos, foros, blogs, landing page, e-commerce. Me considero proactiva, naturalmente curiosa, detallista, y me gusta estar constantemente mejorando mis habilidades.
+      </div>
+      <div id="badges" className="flex flex-wrap">
+        <img alt="ReactJs" src="https://img.shields.io/badge/ReactJs-ffffff?style=for-the-badge&logo=react&logoColor=black" /> <img alt="React Router" src="https://img.shields.io/badge/React_Router-ffffff?style=for-the-badge&logo=react-router&logoColor=black" />
+        <img alt="HTML5" src="https://img.shields.io/badge/HTML5-ffffff?style=for-the-badge&logo=HTML5&logoColor=black" />
+        <img alt="Boostrap" src="https://img.shields.io/badge/Bootstrap-ffffff?style=for-the-badge&logo=bootstrap&logoColor=black" />
+        <img alt="TailwindCSS" src="https://img.shields.io/badge/Tailwind_CSS-ffffff?style=for-the-badge&logo=tailwind-css&logoColor=black" />
+        <img alt="CSS3" src="https://img.shields.io/badge/CSS3-ffffff?style=for-the-badge&logo=css3&logoColor=black" />
+        <img alt="Javascript" src="https://img.shields.io/badge/JavaScript-ffffff?style=for-the-badge&logo=javascript&logoColor=black" /> <img alt="Json" src="https://img.shields.io/badge/json-ffffff?style=for-the-badge&logo=json&logoColor=black" />
+        <img alt="MYSQL" src="https://img.shields.io/badge/MySQL-ffffff?style=for-the-badge&logo=mysql&logoColor=black" /> <img alt="Postgress" src="https://img.shields.io/badge/PostgreSQL-ffffff?style=for-the-badge&logo=PostgreSQL&logoColor=black" />
+        <img alt="Express.js" src="https://img.shields.io/badge/NodeJS-ffffff?style=for-the-badge&logo=Node.JS&logoColor=black" />
+        <img alt="PHP" src="https://img.shields.io/badge/PHP-ffffff?style=for-the-badge&logo=php&logoColor=black" />
+        <img alt="VSCode" src="https://img.shields.io/badge/VSCode-ffffff?style=for-the-badge&logo=visual%20studio%20code&logoColor=black" />
+        <img alt="Adobe Photoshop" src="https://img.shields.io/badge/Adobe%20Photoshop-ffffff?style=for-the-badge&logo=Adobe%20Photoshop&logoColor=black" />
+        <img alt="Google Sheets" src="https://img.shields.io/badge/Google%20Sheets-ffffff?style=for-the-badge&logo=Google%20Sheets&logoColor=black" />
+      </div>
+
     </div>
   )
 }
