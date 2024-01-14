ccc510d5787e719eb7ab2c52787087f266a990d3
Valentina Muñoz <valentinamr@gmail.com>
2024-01-13 21:03:10 -0300

    Actualización de proyectos

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -28,12 +28,12 @@ const Portfolio = () => {
       <div className="flex flex-col gap-3">
         {projects.map((project) => (
           <div id="projects" key={project.id} className="bg-indigo-400 md:flex">
-            <div className="border-b-8 sides text-center"><img alta={project.name} src={project.img} /></div>
-            <div>
+            <div className="border-b-8 sides text-center md:w-2/4"><img alta={project.name} src={project.img} /></div>
+            <div className="md:w-2/4">
               <div className="p-2 uppercase text-center">{project.type} - {project.name}</div>
-            <div className="pb-2 px-2 font-light tracking-wider text-justify">{project.description}. {project.skills.map((skill, index) => (
-              <span key={index} className="me-1">{skill}</span>
-            ))}</div>
+              <div className="pb-2 px-2 font-light tracking-wider text-justify">{project.description}. {project.skills.map((skill, index) => (
+                <span key={index} className="me-1">{skill}</span>
+              ))}</div>
             </div>
           </div>
         ))}
