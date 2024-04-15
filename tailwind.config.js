e3ec5cd0073b22fb2c02aeb8cef5d01940db77fd
Valentina <valentinamr@gmail.com>
2024-04-15 17:53:47 -0400

    Remove flowbite, don't work accordion chevron

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js"
   ],
   theme: {
     extend: {
@@ -16,7 +15,5 @@ export default {
       },
   }
 },
-plugins: [
-    require('flowbite/plugin')
-]
+plugins: []
 }
