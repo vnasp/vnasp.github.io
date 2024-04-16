fb3255bc68833f6701f9930153cfcc1bd6164f73
Valentina <valentinamr@gmail.com>
2024-04-15 22:52:32 -0400

    Add logic to count each skill

--- src/views/Portfolio.jsx
+++ src/views/Portfolio.jsx
@@ -1,14 +1,43 @@
+import {useState} from "react"
 import Projects from "../components/Projects";
 import { ProjectData } from "../data/ProjectsData";
+import { SkillsData } from "../data/SkillsData";
+import Tags from "../components/Tags";
 
 const Portfolio = () => {
+  const countTags = (projects) => {
+    const tagCount = {}
+  
+    projects.forEach(project => {
+      project.tags.forEach(tag => {
+        tagCount[tag] = (tagCount[tag] || 0) + 1;
+      });
+    });
+    return tagCount;
+  };
+  
+  const tagsUsageCount = countTags(ProjectData);
+  console.log(tagsUsageCount);
+
+
+  const [selectedSkill, setSelectedSkill] = useState(null);
+
+  const handleSkillSelect = (skill) => {
+    setSelectedSkill(skill);
+  };
+
+  const filteredProjects = selectedSkill
+    ? ProjectData.filter(project => project.tags.includes(selectedSkill))
+    : ProjectData;
 
   return (
     <main className="main">
-      <section className="w-2/5 about__photo">acá algún filtro</section>
-      <section className="w-3/5 about">
+      <section className="w-2/5">
+        <Tags data={SkillsData} tagCounts={tagsUsageCount} onSkillSelect={handleSkillSelect} selectedSkill={selectedSkill}/>
+      </section>
+      <section className="w-3/5">
         <h3 className="about__title">Proyectos</h3>
-        <Projects data={ProjectData}/>
+        <Projects data={filteredProjects} />
       </section>
     </main>
   );
