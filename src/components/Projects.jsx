e599cd528d24104f9b95b7a8aabe93f45c4506ad
Valentina <valentinamr@gmail.com>
2024-04-15 22:53:14 -0400

    Change styles. Still editing

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,43 +1,59 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Keyboard,
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-} from "swiper/modules";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 const Projects = ({ data }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
   return (
     <Swiper
-      modules={[Keyboard, Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
+      spaceBetween={80}
       slidesPerView={1}
       keyboard={{ enabled: true }}
-      navigation
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
+      pagination={pagination}
+      modules={[Pagination]}
+      className="mySwiper"
     >
       {data.map((project, index) => (
-        <SwiperSlide key={index} className="">
-          <h4 className="skills__type--name">{project.title}</h4>
-          <div className="flex flex-row">
-            <a href={project.github} className="home__button">
-              GitHub
-            </a>
-            <a href={project.demo} className="home__button">
-              Deploy
-            </a>
+        <SwiperSlide key={index} className="flex flex-col">
+          <div className="project__header">
+            <img src={project.image} alt={project.name} className="w-1/4" />
+            <div clasName="flex flex-col">
+              <h4 className="project__name">{project.title}</h4>
+              <div className="inline-block">
+                {project.github && (
+                  <a href={project.github} className="project__button">
+                    GitHub
+                  </a>
+                )}
+                {project.demo && (
+                  <a href={project.demo} className="project__button">
+                    Demo
+                  </a>
+                )}
+              </div>
+            </div>
           </div>
           <div>
-            <img src={project.image} alt={project.about} />
+            <p className="project__desc">{project.about}</p>
+            <div className="project__tags">
+              {project.tags.map((tag, tagIndex) => {
+                return (
+                  <label key={tagIndex} className="project__tags--bg">
+                    {tag}
+                  </label>
+                );
+              })}
+            </div>
           </div>
-          <div>{project.about}</div>
-          <div>{project.tags}</div>
         </SwiperSlide>
       ))}
     </Swiper>
